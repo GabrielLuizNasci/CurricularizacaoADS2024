c415275ee3c6b246fb867f848b336542e5c779f4
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-10-11 19:37:37 -0300

    Criação do Projeto

---
+++ Models/Professor.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CurricularizacaoADS2024.Models
+{
+    [Table("Professores")]
+    public class Professor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        [Display(Name = "Nome: ")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        [Display(Name = "E-mail: ")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Telefone: ")]
+        public int Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "CPF: ")]
+        public int CPF { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        [Display(Name = "E-mail: ")]
+        public string Endereco { get; set; }
+    }
+}
