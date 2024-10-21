dd07233de166f22768633acf26adc1f93bee0d76
Marcel Henrique <marcelhenriquesilva@hotmail.com>
2024-10-21 14:42:06 -0300

    Adicionado Matricula

---
+++ Controllers/MatriculaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurricularizacaoADS2024.Controllers
+{
+    public class MatriculaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
--- obj/Release/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
+++ obj/Release/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurricularizacaoADS2024")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f494f0a6b12549082715792577abcc646c5dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurricularizacaoADS2024")]
