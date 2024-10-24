e59581ca6045d4058d745609d4313585b5b8db3f
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-10-24 13:19:23 -0300

    Criação do Banco de Dados e adição de Id em todos os modelos

--- Models/Turma.cs
+++ Models/Turma.cs
@@ -1,9 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CurricularizacaoADS2024.Models
 {
     [Table("Turmas")]
     public class Turma
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int Id { get; set; }
     }
 }
--- Models/Visita.cs
+++ Models/Visita.cs
@@ -1,9 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CurricularizacaoADS2024.Models
 {
     [Table("Visitas")]
     public class Visita
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int Id { get; set; }
     }
 }
