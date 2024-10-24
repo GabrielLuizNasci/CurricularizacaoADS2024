555bb85ec0c00d42424cb3a01cafa06e197b7a0c
Marcel Henrique <marcelhenriquesilva@hotmail.com>
2024-10-24 16:51:07 -0300

    só deixei em portugues algumas coisas

--- obj/Debug/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243b172fa0e6892ce676bc7839ffa79683288e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8feb3992d79138876a86212dc20f68365ae2b386")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
