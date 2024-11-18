9e1a472e8b6aa7b58ccae75831ee6fd0e3877f72
Marcel Henrique <marcelhenriquesilva@hotmail.com>
2024-11-18 20:33:01 -0300

    fronts add

--- obj/Debug/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/CurricularizacaoADS2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49890506889571acb094c3820be2c7be7d4a1aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d4f0a955ac89e4b05fc815637c20c692af043b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurricularizacaoADS2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
